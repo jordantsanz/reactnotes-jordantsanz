6c91a6567a8d742ad06fe24e3d6f6dfaf43a9661
jordantsanz <jordan.t.sanz.22@dartmouth.edu>
2020-07-13 18:35:21 -0500

    Starter website!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+function counter(addingSeconds) {
+  seconds += 1;
+  $('#main').html(`You've been on this page for ${seconds} seconds...`);
+}
+
+setInterval(counter, 1000);
